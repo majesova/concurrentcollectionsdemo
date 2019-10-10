24a2e2cbdc209eed67362a4edc48cbf975c69c75
MANUEL SOBERANIS <majesova@gmail.com>
2019-10-10 11:02:00 -0500

    Código con invalid operation

---
+++ ConcurrentCollectionsDemo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace ConcurrentCollectionsDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            List<int> listOfIntegers = new List<int> { 1, 2, 3, 4, 5 };
+            //Hilo de ejecución 1
+            new Thread(() => {
+                
+                Console.WriteLine("Adición, thread #{0}", Thread.CurrentThread.GetHashCode());
+                
+                for (int i = 0; i < 1000; i++)//Agrega elementos a la lista
+                    listOfIntegers.Add(i);
+               
+            }).Start();
+            //Hilo de ejecución 2
+            new Thread(() => {
+
+                Console.WriteLine("Recorrido, thread #{0}", Thread.CurrentThread.GetHashCode());
+
+                foreach (var num in listOfIntegers)//Recorre la lista
+                    Console.WriteLine(num);
+
+            }).Start();
+
+            Console.ReadKey();
+        }
+    }
+}
+
+
