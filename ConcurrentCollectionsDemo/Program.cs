917e9975a76f0466f294262cb56e658bbfead434
MANUEL SOBERANIS <majesova@gmail.com>
2019-10-10 11:15:05 -0500

    con lock

--- ConcurrentCollectionsDemo/Program.cs
+++ ConcurrentCollectionsDemo/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Threading;
 
@@ -8,25 +9,27 @@ namespace ConcurrentCollectionsDemo
     {
         static void Main(string[] args)
         {
-            
+            object obj = new object();//Objeto que realiza la exclusión mutua
             List<int> listOfIntegers = new List<int> { 1, 2, 3, 4, 5 };
+
             //Hilo de ejecución 1
             new Thread(() => {
                 
                 Console.WriteLine("Adición, thread #{0}", Thread.CurrentThread.GetHashCode());
-                
-                for (int i = 0; i < 1000; i++)//Agrega elementos a la lista
-                    listOfIntegers.Add(i);
-               
+                lock (obj) { //bloqueo del hilo
+                    for (int i = 0; i < 1000; i++)//Agrega elementos a la lista
+                        listOfIntegers.Add(i);
+                }
+
             }).Start();
             //Hilo de ejecución 2
             new Thread(() => {
 
                 Console.WriteLine("Recorrido, thread #{0}", Thread.CurrentThread.GetHashCode());
-
-                foreach (var num in listOfIntegers)//Recorre la lista
-                    Console.WriteLine(num);
-
+                lock (obj) { //bloqueo del hilo
+                    foreach (var num in listOfIntegers)//Recorre la lista
+                        Console.WriteLine(num);
+                }
             }).Start();
 
             Console.ReadKey();
